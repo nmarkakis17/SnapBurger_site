f699cf18eb64a54fc7906a472d45336ee82e7f6b
nmarkakis17 <nmarkakis@towson.edu>
2025-08-26 11:22:31 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,8 +19,8 @@ export default function NavBar(){
           <img
               src="/images/burger-logo.png"
               alt="SnapBurger logo"
-              width={220}              // intrinsic size (px) – helps prevent layout shift
-              height={220}
+              width={75}              // intrinsic size (px) – helps prevent layout shift
+              height={75}
               style={{ width: 220, height: 220, objectFit: 'contain' }} // CSS control
           />
         </Link>
